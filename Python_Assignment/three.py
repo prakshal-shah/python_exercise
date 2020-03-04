5202532b27a4cfa8cc2fd317cc74d7f287ba3dbc
Prakshal Shah <prakshal.shah@EIC.EINFOCHIPS.COM>
2020-03-04 16:31:08 +0530

    Updated 3rd Assignment

--- Python_Assignment/three.py
+++ Python_Assignment/three.py
@@ -7,13 +7,9 @@ second – filter to limit the specific extension files
 (i.e. function(“c:\test”, “*.txt”) should list all *.txt files from c:\test directory.)
 """
 
-
-
 import os
-# Changed directory for listing all directory
-os.chdir(r"C:\Users\prakshal.shah\eclipse-workspace")  
 
-# function for displaying particular path and containig files
+# function for displaying particular path and containing files
 def dirlist(path,filter):  
     dir_list = []
     is_file_found = 0
@@ -22,21 +18,20 @@ def dirlist(path,filter):
         if file.endswith(filter):
             is_file_found = 1
             # Prints the file name which are ending with given in filter
-			print(os.path.join(path, file))  
+            print(os.path.join(path, file))  
     
-	# If file not found then 
-	if is_file_found != 1:  
+    # If file not found then 
+    if is_file_found != 1:  
         print("No file found in this directory")
-       
+        
     dir_list.append(os.listdir(path))
     os.chdir(path)
     return dir_list
 
-
 # This function checks if the currently working file is in file list or not
 def checkfile(current_file, directory_list):  
   
-	# for loop is for list to find if the current working file is in this directory list or not 
+    # for loop is for list to find if the current working file is in this directory list or not 
     for i in range(len(directory_list)):  
    
         if current_file in directory_list[i]:
@@ -45,16 +40,8 @@ def checkfile(current_file, directory_list):
                 print("\nCurrent working file doesn't exists in this list")
       
 
-print(os.listdir())
-while True:
-    path = input("\nEnter file path")
-  # try and catch if user enter wrong directory then program will not terminate
-    try:
-        print(os.listdir(path))
-        break
-    except FileNotFoundError:
-        print("No such directory")
 
+path = input("\nEnter file path")
 filter = input("\nEnter file extension")  # getting extension from user
 directory_list = dirlist(path, filter)  # calling dirlist function and search for that ending extemsion file
 # print(type(directory_list))     #uncomment for checking the type of directory_list
